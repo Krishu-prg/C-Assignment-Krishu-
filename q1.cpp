474b937ddc0df58d34ea296a143680aed5a2f88a
Krishu <krishushrestha196@gmail.com>
2024-09-04 19:05:43 +0545

    Question Number 1: Reversing First Name and Last Name

---
+++ q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string firstName, lastName;
+
+    cout << "First Name: ";
+    cin >> firstName;
+    
+    cout << "Last Name: ";
+    cin >> lastName;
+
+
+    cout << "Name in reverse: " << lastName << " " << firstName << endl;
+
+    return 0;
+}
