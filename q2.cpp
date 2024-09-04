4da6a321dcd519648326a97484a7565dddee3a0c
Krishu <krishushrestha196@gmail.com>
2024-09-04 19:10:25 +0545

    q2:swapping two variable without third variable

---
+++ q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << " 1st number: ";
+    cin >> num1;
+
+    cout << " 2nd number: ";
+    cin >> num2;
+
+    num1 = num1 + num2; 
+    num2 = num1 - num2; 
+    num1 = num1 - num2; 
+
+    cout << "After swapping the 1st number is: " << num1 << endl;
+    cout << "After swapping the 2nd number is: " << num2 << endl;
+
+    return 0;
+}
